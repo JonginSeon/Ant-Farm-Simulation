3c7576e39f0d0899f0adec502b69a944034fecc8
ElijahRSmith <46466045+ElijahRSmith@users.noreply.github.com>
2019-04-17 18:36:47 -0400

    Javadocs

--- WorldPane.java
+++ WorldPane.java
@@ -4,13 +4,13 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.TilePane;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
  * This class creates and updates an array of multi-colored labels
  * by analyzing the Tile array from an AntFarm object.
+ *
  * @author Elijah, Jongin, Justin
  * @version 4/12/2019
  */
@@ -28,7 +28,12 @@ public class WorldPane extends AnchorPane {
     private Ant queen = new Queen();
     private Timer time;
 
+    /**
+     * Creates a WorldPane with a 100 x 100 array of labels and a
+     * maximum of 10 ants.
+     */
     public WorldPane() {
+
         farm = new AntFarm();
         numberOfAnts = 0;
         count = 0;
@@ -56,10 +61,21 @@ public class WorldPane extends AnchorPane {
         getChildren().add(tilePane);
     }
 
+    /**
+     * Gets the current Antfarm object used.
+     * @return the current Antfarm object.
+     */
     public AntFarm getFarm() {
         return this.farm;
     }
 
+    /**
+     * Creates a TilePane to be added as a child to the WorldPane class.
+     * The TilePane is composed of 101 x 100 colored labels, each representing
+     * a tile type defined by their position. Row 101 is colored white to address
+     * stretching issues.
+     * @param tilePane The TilePane object to be initialized.
+     */
     public void createWorld(TilePane tilePane) {
 
         ants[0] = queen;
@@ -69,9 +85,9 @@ public class WorldPane extends AnchorPane {
         Label lbl;
         for (int r = 0; r < 101; r++) {
             for (int c = 0; c < 100; c++) {
-                lbl= new Label();
+                lbl = new Label();
                 lbl.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                if (r < 10){
+                if (r < 10) {
                     lbl.setStyle("-fx-background-color: DeepSkyBlue");
                 } else if (r == 100) {
                     lbl.setStyle("-fx-background-color: White");
@@ -85,8 +101,12 @@ public class WorldPane extends AnchorPane {
         }
     }
 
-    public void updateWorld(Tile[][] screen)
-    {
+    /**
+     * Updates the WorldPane to reflect a given 100 x100 Tile array. Each tile corresponds
+     * to a unique label color.
+     * @param screen The Tile array that is scanned
+     */
+    public void updateWorld(Tile[][] screen) {
         for (int r = 0; r < 100; r++) {
 
             for (int c = 0; c < 100; c++) {
@@ -95,11 +115,11 @@ public class WorldPane extends AnchorPane {
                 if (screen[r][c] == Tile.S)
                     this.world[r][c].setStyle("-fx-background-color: DeepSkyBlue");
 
-                //only dirt
+                    //only dirt
                 else if (screen[r][c] == Tile.D)
                     this.world[r][c].setStyle("-fx-background-color: Peru");
 
-                //only tile
+                    //only tile
                 else if (screen[r][c] == Tile.T)
                     this.world[r][c].setStyle("-fx-background-color: SaddleBrown");
                 else if (screen[r][c] == Tile.Q)
@@ -117,6 +137,9 @@ public class WorldPane extends AnchorPane {
         }
     }
 
+    /**
+     * Resets the WorldPane to its default coloration and ant amount.
+     */
     public void resetWorld()
     {
         this.farm = new AntFarm();
@@ -130,26 +153,36 @@ public class WorldPane extends AnchorPane {
         updateWorld(farm.getScreen());
     }
 
-    public int getFoodObtained(){
+    /**
+     * Gets the value of the property foodObtained
+     * @return the value of foodObtained
+     */
+    public int getFoodObtained() {
         return foodObtained;
     }
 
-    public void setFoodObtained(int foodObtained){
+    /**
+     * Sets the value of property foodObtained.
+     * @param foodObtained The amount of food collected
+     */
+    public void setFoodObtained(int foodObtained) {
         this.foodObtained = foodObtained;
     }
 
-    public void runSimulation()
-    {
+    /**
+     * Sets a number of actions to a timer that simulate an Ant Farm on the WorldPane
+     * and therefore also on the visible gui.
+     */
+    public void runSimulation() {
         time = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 Behavior antBehavior = new Behavior();
                 Tile currentAnt;
-                for (int i = 0; i < numberOfAnts; i++)
-                {
+                for (int i = 0; i < numberOfAnts; i++) {
                     currentAnt = ants[i].getAntTile();
-                    switch(currentAnt) {
+                    switch (currentAnt) {
 
                         case Q:
                             antBehavior.digToBottom((Queen) ants[i], farm.getScreen());
@@ -169,47 +202,53 @@ public class WorldPane extends AnchorPane {
 
                     }
                 }
-                if(antBehavior.getFoodObtained()==1 ||antBehavior.getFoodObtained()==2 ||antBehavior.getFoodObtained()==3){
+                if (antBehavior.getFoodObtained() == 1 || antBehavior.getFoodObtained() == 2 || antBehavior.getFoodObtained() == 3) {
                     foodObtained++;
                 }
 
                 count++;
-                if(foodObtained >30) {
+                if (foodObtained > 30) {
                     if (count % 10 == 0)
                         antBehavior.foodGenerator(farm.getScreen());
-                }
-                else if (foodObtained<=30){
+                } else if (foodObtained <= 30) {
                     if (count % 5 == 0)
                         antBehavior.foodGenerator(farm.getScreen());
-
                 }
 
                 updateWorld(farm.getScreen());
             }
         };
-            time.schedule(task, 0, farm.getPlayspeed());
+        time.schedule(task, 0, farm.getPlayspeed());
     }
 
-
-
-    public void update(){
+    /**
+     * Updates the world once using the currently defined AntFarm object.
+     */
+    public void update() {
         updateWorld(farm.getScreen());
     }
 
+    /**
+     * Gets the current Ant array.
+     * @return All the active ants in the simulation
+     */
     public Ant[] getAnts() {
         return ants;
     }
 
+    /**
+     * Gets the number of ant in the simulation.
+     * @return The number of Ants
+     */
     public int getNumberOfAnts() {
         return numberOfAnts;
     }
 
-    public void setNumberOfAnts(int numberOfAnts) {
-        this.numberOfAnts = numberOfAnts;
-    }
-
-    public void stopSimulation()
-    {
+    /**
+     * Stop the timer if it is running in runSimulation() and therefore
+     * stop the simulation.
+     */
+    public void stopSimulation() {
         time.cancel();
     }
 
