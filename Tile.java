a76f6c146625eb3c9d91ae08ca420e0c6519f534
ElijahRSmith <46466045+ElijahRSmith@users.noreply.github.com>
2019-04-17 23:29:15 -0400

    Javadocs

--- Tile.java
+++ Tile.java
@@ -4,8 +4,9 @@ package main;
  * This enum represents the different tiles that will need to be
  * displayed by the AntFarm gui. Each letter stands for a different
  * type of tile.
- * S = Sky, D = Dirt, T = Tunnel, A = Ant
+ * S = Sky, D = Dirt, T = Tunnel, G = DiggingAnt, W = WorkingAnt,
+ * K = King, Q = Queen, F = Food
  * @author Justin
- * @version 4/06/2019
+ * @version 4/17/2019
  */
 enum Tile {S, D, T, G, W, K, Q, F}
