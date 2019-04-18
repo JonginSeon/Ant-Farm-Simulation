9054a724257ed01fb50ad193221c944f93b3db78
ElijahRSmith <46466045+ElijahRSmith@users.noreply.github.com>
2019-04-18 00:16:09 -0400

    Javadocs (Incomplete)
    I don't understand the difference between the many similar moveRandom methods. The rest is done besides that.

--- Behavior.java
+++ Behavior.java
@@ -1,10 +1,24 @@
 package main;
 
 import java.util.Random;
-public class Behavior {
-    Random rn = new Random();
-int foodObtained;
 
+/**
+ * This class handles the movements and behaviors of each different Ant Type.
+ * It also controls the generation of food and the effects of an Ant touching
+ * food tiles.
+ */
+public class Behavior { //TODO Javadocs are NOT DONE for moveRandom methods!
+
+    Random rn = new Random();
+    int foodObtained;
+
+    /**
+     * Makes an Ant randomly shift position to an adjacent position in the world.
+     * There are
+     *
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     */
     public void moveRandomCross(Ant ant, Tile[][] screen) {
         int direction = rn.nextInt(4) + 1;
         switch (direction) {
@@ -16,7 +30,7 @@ int foodObtained;
                     break;
                 }
                 screen[ant.getLocX() + 1][ant.getLocY()] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
@@ -28,7 +42,7 @@ int foodObtained;
                     break;
                 }
                 screen[ant.getLocX()][ant.getLocY() - 1] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
@@ -40,7 +54,7 @@ int foodObtained;
                     break;
                 }
                 screen[ant.getLocX() - 1][ant.getLocY()] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
@@ -52,11 +66,18 @@ int foodObtained;
                     break;
                 }
                 screen[ant.getLocX()][ant.getLocY() + 1] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
         }
     }
+
+    /**
+     * Makes an Ant randomly shift position to an adjacent position.
+     * There are four possible movements: up, down, left, and right.
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     */
     public void moveRandom(Ant ant, Tile[][] screen) {
 
         int direction = rn.nextInt(4) + 1;
@@ -64,66 +85,73 @@ int foodObtained;
         switch (direction) {
             case 1:
                 ant.setLocX(ant.getLocX() - 1);
-                if (antOutOfBounds(ant,screen)) {
+                if (antOutOfBounds(ant, screen)) {
                     ant.setLocX(ant.getLocX() + 1);
-                    moveRandom(ant,screen);
+                    moveRandom(ant, screen);
                     break;
                 }
                 screen[ant.getLocX() + 1][ant.getLocY()] = Tile.T;
-                screen[ant.getLocX()][ant.getLocY()] =ant.getAntTile();
+                screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
             case 2:
                 ant.setLocY(ant.getLocY() + 1);
-                if (antOutOfBounds(ant,screen)) {
+                if (antOutOfBounds(ant, screen)) {
                     ant.setLocY(ant.getLocY() - 1);
-                    moveRandom(ant,screen);
+                    moveRandom(ant, screen);
                     break;
                 }
                 screen[ant.getLocX()][ant.getLocY() - 1] = Tile.T;
-                screen[ant.getLocX()][ant.getLocY()] =ant.getAntTile();
+                screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
             case 3:
                 ant.setLocX(ant.getLocX() + 1);
-                if (antOutOfBounds(ant,screen)) {
+                if (antOutOfBounds(ant, screen)) {
                     ant.setLocX(ant.getLocX() - 1);
-                    moveRandom(ant,screen);
+                    moveRandom(ant, screen);
                     break;
                 }
                 screen[ant.getLocX() - 1][ant.getLocY()] = Tile.T;
-                screen[ant.getLocX()][ant.getLocY()] =ant.getAntTile();
+                screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
             case 4:
                 ant.setLocY(ant.getLocY() - 1);
-                if (antOutOfBounds(ant,screen)) {
+                if (antOutOfBounds(ant, screen)) {
                     ant.setLocY(ant.getLocY() + 1);
-                    moveRandom(ant,screen);
+                    moveRandom(ant, screen);
                     break;
                 }
                 screen[ant.getLocX()][ant.getLocY() + 1] = Tile.T;
-                screen[ant.getLocX()][ant.getLocY()] =ant.getAntTile();
+                screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
             default:
                 break;
         }
     }
+
+    /**
+     * Makes an Ant randomly shift position to an adjacent position in the world.
+     * There are four possible movements: up-left, up-right, down-left, and down-right
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     */
     public void moveRandomDiag(Ant ant, Tile[][] screen) {
         int direction = rn.nextInt(4) + 1;
         switch (direction) {
             case 1:
                 ant.setLocX(ant.getLocX() - 1);
                 ant.setLocY(ant.getLocY() - 1);
-                if (antOutOfBounds(ant, screen)){
+                if (antOutOfBounds(ant, screen)) {
                     ant.setLocX(ant.getLocX() + 1);
                     ant.setLocY(ant.getLocY() + 1);
                     moveRandomDiag(ant, screen);
                     break;
                 }
                 screen[ant.getLocX() + 1][ant.getLocY() + 1] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
@@ -133,7 +161,7 @@ int foodObtained;
                 if (moveBackNE(ant, screen)) break;
 
                 screen[ant.getLocX() + 1][ant.getLocY() - 1] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
@@ -147,7 +175,7 @@ int foodObtained;
                     break;
                 }
                 screen[ant.getLocX() - 1][ant.getLocY() + 1] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
 
@@ -157,14 +185,22 @@ int foodObtained;
                 if (moveBackNW(ant, screen)) break;
 
                 screen[ant.getLocX() - 1][ant.getLocY() - 1] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                 break;
         }
     }
 
+    /**
+     * Makes the ant move vertically down  in random horizontal
+     * directions until it reaches row 70. At Row 70, the ant stops going down,
+     * its current position is set as the location of the nest, and finally
+     * buildHive() is called to create the nest.
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     */
     public void digToBottom(Queen ant, Tile[][] screen) {
-        if (ant.getLocX() < 70) {
+        if (ant.getLocX() < 71) {
             int direction = rn.nextInt(3) + 1;
             switch (direction) {
                 case 1:
@@ -173,7 +209,7 @@ int foodObtained;
                     if (moveBackNE(ant, screen)) break;
 
                     screen[ant.getLocX() - 1][ant.getLocY() + 1] = Tile.T;
-                    isFood(ant,screen);
+                    isFood(ant, screen);
                     screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                     break;
 
@@ -186,7 +222,7 @@ int foodObtained;
                     }
 
                     screen[ant.getLocX() - 1][ant.getLocY()] = Tile.T;
-                    isFood(ant,screen);
+                    isFood(ant, screen);
                     screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                     break;
 
@@ -196,19 +232,24 @@ int foodObtained;
                     if (moveBackNW(ant, screen)) break;
 
                     screen[ant.getLocX() - 1][ant.getLocY() - 1] = Tile.T;
-                    isFood(ant,screen);
+                    isFood(ant, screen);
                     screen[ant.getLocX()][ant.getLocY()] = ant.getAntTile();
                     break;
             }
-        }
-        else if (ant.getNestCenterX() == -1) {
+
+        } else if (ant.getNestCenterX() == -1) {
             ant.setNestCenterX();
             ant.setNestCenterY();
             buildHive(ant, screen);
-        }
-        else buildHive(ant, screen);
+        } else buildHive(ant, screen);
     }
 
+    /**
+     * Makes an ant move a randomly move around its nest area, creating a small,
+     * dug out area. From then on, new Ants will spawn in the nest area.
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     */
     private void buildHive(Queen ant, Tile[][] screen) {
         int prevX;
         int prevY;
@@ -217,52 +258,54 @@ int foodObtained;
                 ant.setLocY(ant.getLocY() - 1);
 
                 screen[ant.getNestCenterX()][ant.getNestCenterY()] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = Tile.Q;
-            }
-            else if (screen[ant.getLocX() - 1][ant.getLocY() - 1] == Tile.D) {
+            } else if (screen[ant.getLocX() - 1][ant.getLocY() - 1] == Tile.D) {
                 ant.setLocX(ant.getLocX() - 1);
                 ant.setLocY(ant.getLocY() - 1);
 
 
                 screen[ant.getNestCenterX()][ant.getNestCenterY()] = Tile.T;
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getLocX()][ant.getLocY()] = Tile.Q;
-            }
-            else if (screen[ant.getLocX() - 1][ant.getLocY()] == Tile.D) {
+            } else if (screen[ant.getLocX() - 1][ant.getLocY()] == Tile.D) {
                 ant.setLocX(ant.getLocX() - 1);
 
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getNestCenterX()][ant.getNestCenterY()] = Tile.T;
                 screen[ant.getLocX()][ant.getLocY()] = Tile.Q;
-            }
-            else if (screen[ant.getLocX() - 1][ant.getLocY() + 1] == Tile.D) {
+            } else if (screen[ant.getLocX() - 1][ant.getLocY() + 1] == Tile.D) {
                 ant.setLocX(ant.getLocX() - 1);
                 ant.setLocY(ant.getLocY() + 1);
 
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getNestCenterX()][ant.getNestCenterY()] = Tile.T;
                 screen[ant.getLocX()][ant.getLocY()] = Tile.Q;
-            }
-            else if (screen[ant.getLocX()][ant.getLocY() + 1] == Tile.D)  {
+            } else if (screen[ant.getLocX()][ant.getLocY() + 1] == Tile.D) {
                 ant.setLocY(ant.getLocY() + 1);
 
-                isFood(ant,screen);
+                isFood(ant, screen);
                 screen[ant.getNestCenterX()][ant.getNestCenterY()] = Tile.T;
                 screen[ant.getLocX()][ant.getLocY()] = Tile.Q;
             }
-        }
-        else {
+        } else {
             prevX = ant.getLocX();
             prevY = ant.getLocY();
             ant.setLocX(ant.getNestCenterX());
             ant.setLocY(ant.getNestCenterY());
-            isFood(ant,screen);
+            isFood(ant, screen);
             screen[prevX][prevY] = Tile.T;
             screen[ant.getLocX()][ant.getLocY()] = Tile.Q;
         }
     }
 
+    /**
+     * Moves an Ant away from the world boundary in a north-west direction.
+     * This only happens if the ant is currently out of bounds.
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     * @return True if the Ant was out of bounds, False if not.
+     */
     private boolean moveBackNW(Ant ant, Tile[][] screen) {
         if (antOutOfBounds(ant, screen)) {
             ant.setLocX(ant.getLocX() - 1);
@@ -273,6 +316,13 @@ int foodObtained;
         return false;
     }
 
+    /**
+     * Moves an Ant away from the world boundary in a north-east direction.
+     * This only happens if the ant is currently out of bounds.
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     * @return True if the Ant was moved back into its bounds, False if not.
+     */
     private boolean moveBackNE(Ant ant, Tile[][] screen) {
         if (antOutOfBounds(ant, screen)) {
             ant.setLocX(ant.getLocX() - 1);
@@ -284,12 +334,19 @@ int foodObtained;
     }
 
 
+    /**
+     * Returns whether the location of an Ant is currently outside
+     * of the world bounds.
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     * @return True if the Ant was out of bounds, False if not.
+     */
     private boolean antOutOfBounds(Ant ant, Tile[][] screen) {
-        if (ant.getLocX() < 0 || ant.getLocX() > 99) //Used to be 100
+        if (ant.getLocX() < 0 || ant.getLocX() > 100) //Used to be 100
         {
             return true;
         }
-        if (ant.getLocY() < 0 || ant.getLocY() > 99) {
+        if (ant.getLocY() < 0 || ant.getLocY() > 100) {
             return true;
         }
         if (screen[ant.getLocX()][ant.getLocY()] == Tile.S) {
@@ -299,39 +356,56 @@ int foodObtained;
 
     }
 
-    private boolean antOutOfBoundsForG(Ant ant, Tile[][] screen){
+//    private boolean antOutOfBoundsForG(Ant ant, Tile[][] screen) {
+//
+//        if (screen[ant.getLocX()][ant.getLocY()] == Tile.S) {
+//            return true;
+//        }
+//
+//        if (screen[ant.getLocX()][ant.getLocY()] == Tile.T) {
+//            return true;
+//        }
+//        return screen[ant.getLocX()][ant.getLocY()] == Tile.Q || screen[ant.getLocX()][ant.getLocY()] == Tile.G || screen[ant.getLocX()][ant.getLocY()] == Tile.W;
+//
+//
+//    }
 
-        if (screen[ant.getLocX()][ant.getLocY()] == Tile.S) {
-            return true;
-        }
-
-        if (screen[ant.getLocX()][ant.getLocY()] == Tile.T) {
-            return true;
-        }
-        return screen[ant.getLocX()][ant.getLocY()] == Tile.Q || screen[ant.getLocX()][ant.getLocY()] == Tile.G || screen[ant.getLocX()][ant.getLocY()] == Tile.W;
-
-
-    }
-
-    public void foodGenerator( Tile[][] screen){
+    /**
+     * Randomly turns one non-Ant, non-Sky Tile into a food Tile
+     * @param screen The current 100 x 100 Tile array
+     */
+    public void foodGenerator(Tile[][] screen) {
         Random rn = new Random();
-            int x = rn.nextInt(89) + 10;
-            int y = rn.nextInt(99) + 1;
+        int x = rn.nextInt(89) + 10;
+        int y = rn.nextInt(99) + 1;
 
-            if (x < 100 && y < 100) {
-                if (screen[x][y] == Tile.F) foodGenerator(screen);
-                else screen[x][y] = Tile.F;
-            }
+        if (x < 100 && y < 100)
+        {
+            if (screen[x][y] == Tile.F)
+                foodGenerator(screen);
+            else
+                screen[x][y] = Tile.F;
+        }
     }
 
-    public int isFood(Ant ant, Tile[][] screen){
-        if(screen[ant.getLocX()][ant.getLocY()]==Tile.F){
+    /**
+     * Returns whether an Ant is occupying a space with a Food Tile.
+     * @param ant The ant that will be moved.
+     * @param screen The array the ant moves through.
+     * @return True if food was obtained, False if not.
+     */
+    public int isFood(Ant ant, Tile[][] screen) {
+        if (screen[ant.getLocX()][ant.getLocY()] == Tile.F) {
             this.foodObtained++;
         }
         return this.foodObtained;
     }
 
-    public int getFoodObtained(){
+    /**
+     * Gets the amount of food obtained this round.
+     * @return The total amount of food found.
+     */
+    public int getFoodObtained() {
 
         return foodObtained;
 
