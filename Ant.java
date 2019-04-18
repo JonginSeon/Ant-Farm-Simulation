1f8c22d35b6fa8ab643d6d8198b140031aa3ae52
ElijahRSmith <46466045+ElijahRSmith@users.noreply.github.com>
2019-04-18 16:55:31 -0400

    Javadocs

--- Ant.java
+++ Ant.java
@@ -1,14 +1,40 @@
 package main;
 
 /**
- * Establishes the minimum functionality for all Ants, including WorkingAnts, DiggingAnts,
- * Kings, and Queens.
+ * Establishes the minimum functionality for all Ants, including WorkingAnts,
+ * DiggingAnts, Kings, and Queens.
  */
-public interface  Ant{
+public interface  Ant
+{
 
-   void setLocX(int locX);
-   void setLocY(int locY);
-   int getLocX();
-   int getLocY();
-   Tile getAntTile();
+   /**
+    * Sets the X-coordinate for this ant
+    * @param locX The new X-coordinate
+    */
+   public void setLocX(int locX);
+
+   /**
+    * Sets the Y-coordinate for this ant
+    * @param locY The new Y-coordinate
+    */
+   public void setLocY(int locY);
+
+   /**
+    * Gets the X-coordinate for this ant
+    * @return The X-coordinate
+    */
+   public int getLocX();
+
+   /**
+    * Gets the Y-coordinate for this ant
+    * @return The Y-coordinate
+    */
+   public int getLocY();
+
+   /**
+    * Gets the Tile type for this ant
+    * @return This ant's Tile type
+    */
+   public Tile getAntTile();
 }
+
